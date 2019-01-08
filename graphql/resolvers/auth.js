2ea48fc09f8fcef4bcf1614bae1121909814a252
sleeplesseditor <josh@bryson.me.uk>
2019-01-08 17:49:57 +0000

    Initial authentication setup

--- graphql/resolvers/auth.js
+++ graphql/resolvers/auth.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../../models/user');
 
 module.exports = {
@@ -22,5 +23,23 @@ module.exports = {
         } catch {
             throw err;
         }
+    },
+    login: async ({ email, password }) => {
+        const user = await User.findOne({ email: email });
+        if (!user) {
+            throw new Error('User does not exist');
+        }
+        const isEqual = await bcrypt.compare(password, user.password);
+        if (!isEqual) {
+            throw new Error('Invalid credentials');
+        }
+        const token = jwt.sign({userId: user.id, email: user.email}, 'asupersecretkey', {
+            expiresIn: '1h'
+        });
+        return { 
+            userId: user.id, 
+            token: token, 
+            tokenExpiration: 1 
+        };
     }
 }
