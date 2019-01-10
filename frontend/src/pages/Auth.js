e65d720fcd931393190e949dacc8765b2988fd71
sleeplesseditor <josh@bryson.me.uk>
2019-01-10 19:34:29 +0000

    Auth page form styling

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import './Auth.css';
 
 class AuthPage extends Component {
     render() {
         return (
-            <div>
-                <h1>Auth Page</h1>
-            </div>
+            <form className="auth-form">
+                <div className="form-control">
+                    <label htmlFor="email">Email</label>
+                    <input 
+                        type="email" 
+                        id="email"
+                    />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="password">Password</label>
+                    <input 
+                        type="password" 
+                        id="password"
+                    />
+                </div>
+                <div className="form-actions">
+                    <button type="submit">Submit</button>
+                    <button type="button">Switch to Sign Up</button>
+                </div>
+            </form>
         );
     }
 }
