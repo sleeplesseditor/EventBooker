a7767be3d0b8fdae5c0ca9be17a3054d596e745b
sleeplesseditor <josh@bryson.me.uk>
2019-01-15 17:38:36 +0000

    JwT token and routing updated

--- frontend/src/components/Navigation/MainNavigation.js
+++ frontend/src/components/Navigation/MainNavigation.js
@@ -1,34 +1,41 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import AuthContext from '../../context/auth-context';
 import './MainNavigation.css';
 
 const MainNavigation = props => (
-    <header className="main-navigation">
-        <div className="main-navigation__logo">
-            <h1>EventBooker</h1>
-        </div>
-        <div className="main-navigation__menu">
-            <nav className="main-navigation__items">
-                <ul>
-                    <li>
-                        <NavLink to="/auth">
-                            Log In
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/events">
-                            Events
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/bookings">
-                            Bookings
-                        </NavLink>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    </header>
+    <AuthContext.Consumer>
+        {(context) => {
+            return (
+                <header className="main-navigation">
+                    <div className="main-navigation__logo">
+                        <h1>EventBooker</h1>
+                    </div>
+                    <div className="main-navigation__menu">
+                        <nav className="main-navigation__items">
+                            <ul>
+                                {!context.token && <li>
+                                    <NavLink to="/auth">
+                                        Log In
+                                    </NavLink>
+                                </li>}
+                                <li>
+                                    <NavLink to="/events">
+                                        Events
+                                    </NavLink>
+                                </li>
+                                {context.token && <li>
+                                    <NavLink to="/bookings">
+                                        Bookings
+                                    </NavLink>
+                                </li>}
+                            </ul>
+                        </nav>
+                    </div>
+                </header>
+            )
+        }}
+    </AuthContext.Consumer>
 );
 
 export default MainNavigation;
