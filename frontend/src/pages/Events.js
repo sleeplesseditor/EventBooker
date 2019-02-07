972589a411946f69471f76881ffb095ad7412569
sleeplesseditor <josh@bryson.me.uk>
2019-02-07 23:58:05 +0000

    Minor bug fixing and query improvements

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -59,8 +59,8 @@ class EventsPage extends Component {
 
         const requestBody = {
             query: `
-                mutation {
-                    createEvent(eventInput: {title: "${title}", description: "${description}", price: ${price}, date: "${date}"}) {
+                mutation CreateEvent($title: String!, $description: String!, $price: Float!, $date: String!){
+                    createEvent(eventInput: {title: $title, description: $description, price: $price, date: $date}) {
                         _id
                         title
                         description
@@ -68,7 +68,13 @@ class EventsPage extends Component {
                         price
                     }
                 }
-            `
+            `,
+            variables: {
+                title: title,
+                desc: description,
+                price: price,
+                date: date
+            }
         };
 
         const token = this.context.token;
@@ -180,14 +186,17 @@ class EventsPage extends Component {
 
         const requestBody = {
             query: `
-                mutation {
-                    bookEvent(eventId: "${this.state.selectedEvent._id}") {
+                mutation BookEvent($id: ID!){
+                    bookEvent(eventId: $id) {
                         _id
                         createdAt
                         updatedAt
                     }
                 }
-            `
+            `,
+            variables: {
+                id: this.state.selectedEvent._id
+            }
         };
 
         fetch('http://localhost:8000/graphql', {
